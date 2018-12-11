d46ec7b23911c81cc3e7db926085d5db7bd093db
fgilan <kevinseto24@gmail.com>
2018-12-11 20:40:09 +0900

    updated the data collecting method to stop at the most recently collected date

--- get_data_newsup.py
+++ get_data_newsup.py
@@ -4,7 +4,7 @@ import time
 import csv
 import re
 
-url = "https://www3.nhk.or.jp/news/netnewsup/?utm_int=all_header_menu_netnewsup#!/12/"
+url = "https://www3.nhk.or.jp/news/netnewsup/?utm_int=all_header_menu_netnewsup"
 driver = webdriver.Chrome()
 driver.get(url)
 time.sleep(5)
@@ -24,33 +24,43 @@ content_items = content_items_1 + content_items_2
 for content_item in content_items:
     for item in content_item.find_elements_by_tag_name('a'):
         links.append(item.get_attribute('href'))
+#get the most recent date already in the database
+with open('nhk_articles_newsup.csv', 'r', encoding='utf-8') as file:
+    reader = csv.reader(file)
+    recent_date = list(reader)[-1][1]
 
-with open('nhk_articles_newsup.csv', 'a') as file:
+#retrive info and write to file
+with open('nhk_articles_newsup.csv', 'a', encoding='UTF-8') as file:
+    #list of info to add
+    articles = []
     writer = csv.writer(file, delimiter=',', lineterminator='\n')
     for link in links:
-        try:
-            info = []
-            driver.get(link)
-            time.sleep(2)
-            #get title
-            title = driver.find_element_by_class_name('content--title')
-            title = title.find_elements_by_tag_name('span')[1].text
-            info.append(title)
-            #get date
-            date = driver.find_elements_by_tag_name('time')
-            date = date[2].text.split()[0]
-            info.append(date)
-            #get summary
-            body_text = []
-            summary = driver.find_element_by_class_name('content--summary').text
-            body_text.append(re.sub('\n','', summary))
-            #get body paragraphs
-            pars = driver.find_elements_by_class_name('body-text')
-            for p in pars:
-                body_text.append(re.sub('\n', '', p.text))
-            info.append(''.join(body_text))
-            writer.writerow(info)
-        except:
-            continue
+        info = []
+        driver.get(link)
+        time.sleep(2)
+        #get title
+        title = driver.find_element_by_class_name('content--title')
+        title = title.find_elements_by_tag_name('span')[1].text
+        info.append(title)
+        #get date
+        date = driver.find_elements_by_tag_name('time')
+        date = date[2].text.split()[0]
+        #if we have already gathered data for this date, end collection
+        if date == recent_date:
+            break
+        info.append(date)
+        #get summary
+        body_text = []
+        summary = driver.find_element_by_class_name('content--summary').text
+        body_text.append(re.sub('\n','', summary))
+        #get body paragraphs
+        pars = driver.find_elements_by_class_name('body-text')
+        for p in pars:
+            body_text.append(re.sub('\n', '', p.text))
+        info.append(''.join(body_text))
+        articles.append(info)
+    for article in articles[::-1]:
+        writer.writerow(article)
+
 driver.close()
 driver.quit()
--- get_data_toku.py
+++ get_data_toku.py
@@ -20,33 +20,42 @@ for x in header_articles:
 content_items = sec.find_element_by_class_name('content--items')
 for x in content_items.find_elements_by_tag_name('a'):
     links.append(x.get_attribute('href'))
-
-with open('nhk_articles_toku.csv', 'a') as file:
+#get most recent date of articles gathered
+with open('nhk_articles_toku.csv', 'r', encoding='utf-8') as file:
+    reader = csv.reader(file)
+    recent_date = list(reader)[-1][1]
+with open('nhk_articles_toku.csv', 'a', encoding='utf-8') as file:
+    articles = []
     writer = csv.writer(file, delimiter=',', lineterminator='\n')
     for link in links:
-        try:
-            info = []
-            driver.get(link)
-            time.sleep(2)
-            #get title
-            title = driver.find_element_by_class_name('content--title')
-            title = title.find_elements_by_tag_name('span')[1].text
-            info.append(title)
-            #get date
-            date = driver.find_elements_by_tag_name('time')
+        info = []
+        driver.get(link)
+        time.sleep(2)
+        #get title
+        title = driver.find_element_by_class_name('content--title')
+        title = title.find_elements_by_tag_name('span')[1].text
+        info.append(title)
+        #get date
+        date = driver.find_elements_by_tag_name('time')
+        if date[1].text == '':
+            date = date[2].text.split()[0]
+        else:
             date = date[1].text.split()[0]
-            info.append(date)
-            #get summary
-            body_text = []
-            summary = re.sub('\n','',driver.find_element_by_class_name('content--summary').text)
-            body_text.append(summary)
-            #get body paragraphs
-            pars = driver.find_elements_by_class_name('body_text')
-            for p in pars:
-                body_text.append(re.sub('\n', '', p.text))
-            info.append(''.join(body_text))
-            writer.writerow(info)
-        except:
-            continue
+        if date == recent_date:
+            break
+        info.append(date)
+        #get summary
+        body_text = []
+        summary = re.sub('\n','',driver.find_element_by_class_name('content--summary').text)
+        body_text.append(summary)
+        #get body paragraphs
+        pars = driver.find_elements_by_class_name('body_text')
+        for p in pars:
+            body_text.append(re.sub('\n', '', p.text))
+        info.append(''.join(body_text))
+        articles.append(info)
+    for article in articles[::-1]:
+        writer.writerow(article)
+
 driver.close()
 driver.quit()
