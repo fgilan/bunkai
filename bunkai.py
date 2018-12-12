a4323b8b70ed03460c56df78bf8750e4f83c74ef
fgilan <kevinseto24@gmail.com>
2018-12-12 17:29:41 +0900

    implemented common combinations

--- bunkai.py
+++ bunkai.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
+import MeCab
+import collections
 
 app = Flask(__name__)
 
@@ -13,6 +15,28 @@ articles = articles1 + articles2 + articles3
 article_count = str(len(articles))
 message = '検索したい言葉を入力してください。現在の記事数　' + article_count
 
+def search(word):
+    hits = {}
+    for article in articles:
+        sentences = article.split("。")
+        for i in range(len(sentences)):
+            if word in sentences[i]:
+                #store the index of the word for bolding later
+                idx = sentences[i].index(word)
+                hits[sentences[i] + "。"] = idx
+    return hits
+
+def patterns(hits, word):
+    p = MeCab.Tagger('-Owakati')
+    counter = collections.Counter()
+    avoid = [w for w in word] + [word] + ['\n', '']
+    for sentence in hits.keys():
+        #get the part of the sentence surrounding the word
+        neighborhood = sentence[max(0, hits[sentence] - 5) : min(len(sentence) - 1, hits[sentence] + len(word) + 5)]
+        #parse the neighborhood and udpate the counter
+        counter.update([w for w in list(p.parse(neighborhood).split()) if w not in avoid and len(w) >= 2])
+    return counter.most_common(5)
+
 @app.route('/')
 def index():
     title = 'ようこそ'
@@ -24,8 +48,9 @@ def post():
     if request.method == "POST":
         word = request.form['word']
         hits = search(word)
+        pat = patterns(hits, word)
         return render_template('index.html', message = message, word = word, title = title,
-            hits = hits, hits_number = len(hits))
+            hits = hits, hits_number = len(hits), patterns = pat)
     else:
         return redirect(url_for('index'))
 
@@ -33,15 +58,7 @@ def post():
 def not_found(error):
     return redirect(url_for('index'))
 
-def search(word):
-    hits = {}
-    for article in articles:
-        sentences = article.split("。")
-        for i in range(len(sentences)):
-            if word in sentences[i]:
-                idx = sentences[i].index(word)
-                hits[sentences[i] + "。"] = idx
-    return hits
+
 
 if __name__ == '__main__':
     app.debug = True
